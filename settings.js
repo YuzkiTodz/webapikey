69be1e1f83d578d63ac2122c93ddc19f292085c1
YuzakiTodz <97385500+YuzkiTodz@users.noreply.github.com>
2023-01-06 10:09:22 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc' 
-global.apikey = ["Zeeoneofc", "Alphabot", "Alpha"]
+global.creator = 'Tsuyuzaki' 
+global.apikey = ["Tsuyuzaki"]
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
